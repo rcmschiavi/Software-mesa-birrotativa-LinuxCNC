e7d685df19a46efc6687825168b9a0d12fecedb2
rcmschiavi <rodolfocmschiavi@gmail.com>
2019-12-07 15:09:16 -0300

    Defines dos eixos

--- BBB/BBB_python/DT_controller/MACHINE_CONTROLL.py
+++ BBB/BBB_python/DT_controller/MACHINE_CONTROLL.py
@@ -2,21 +2,36 @@
 import hal, time
 
 class Machine_control:
+    axisBasc = {
+        "homeSpeed": 3,
+        "homeSpeedFine": 1,
+        "axisIndex": 1
+    }
+    axisRot = {
+        "homeSpeed": 25,
+        "homeSpeedFine": 10,
+        "axisIndex": 0
+    }
+
     def __init__(self):
         self.h = hal.component("machine_controll")
-        self.maxvel_basc = 25 #Graus/s
-        self.maxvel_rot = 3
+        self.maxvel_basc = 3 #Graus/s
+        self.maxvel_rot = 25
         self.maxAccel_basc = 10 #Graus/s^2
         self.maxAccel_rot = 10
+        self.BASC_AXIS = 1
+        self.ROT_AXIS = 0
         self.init_pins()
         self.init_params()
-        self.roda()
+
+    def axis
 
     def init_pins(self):
         self.h.newpin("set_position_basc", hal.HAL_FLOAT, hal.HAL_OUT)
         self.h.newpin("set_position_rot", hal.HAL_FLOAT, hal.HAL_OUT)
         self.h.newpin("enable_basc", hal.HAL_BIT, hal.HAL_OUT)
         self.h.newpin("enable_rot", hal.HAL_BIT, hal.HAL_OUT)
+        #Não peguei qual sensor é qual
         self.h.newpin("sensor_basc", hal.HAL_BIT, hal.HAL_IN)
         self.h.newpin("sensor_rot", hal.HAL_BIT, hal.HAL_IN)
         self.h.newpin("get_position_basc", hal.HAL_FLOAT, hal.HAL_IN)
@@ -28,8 +43,12 @@ class Machine_control:
         hal.set_p("stepgen.0.maxaccel", self.maxAccel_basc)
         hal.set_p("stepgen.1.maxaccel", self.maxAccel_rot)
 
-    def setSpeed(self, speed_basc, speed_rot):
+    def calcSpeed(self,speed):
+
+
+    def setSpeed(self, speed):
         #Lógica para que a velocidade em cada eixo seja equivalente ao movimento
+        speed_basc, speed_rot = self.calcSpeed(speed)
         hal.set_p("stepgen.0.maxvel", speed_basc) #Precisa ser uma string
         hal.set_p("stepgen.1.maxvel", speed_rot)
 
@@ -39,6 +58,7 @@ class Machine_control:
         return [basc_pos,rot_pos]
 
     def setMachinePos(self,position_basc,position_rot, speed):
+        self.setSpeed(speed)
         self.h["set_position_basc"] = position_basc
         self.h["set_position_rot"] = position_rot
         return
@@ -46,19 +66,27 @@ class Machine_control:
     def setAxisPos(self,axis,pos,speed):
         #Seria interessante ter a velocidade aqui
         if axis==0:
-            self.h["set_position_basc"] = pos
-            hal.set_p("stepgen.1.maxvel", speed)
-        elif axis==1:
             self.h["set_position_rot"] = pos
             hal.set_p("stepgen.0.maxvel", speed)
+        elif axis==1:
+            self.h["set_position_basc"] = pos
+            hal.set_p("stepgen.1.maxvel", speed)
         return
 
+    def stop_axis(self, axis):
+        if axis==self.ROT_AXIS:
+            hal.h["enable_rot"] = False
+            self.h["set_position_rot"] = self.h["get_position_rot"]
+        elif axis==self.BASC_AXIS:
+            hal.h["enable_basc"] = False
+            self.h["set_position_basc"] = self.h["get_position_basc"]
+        return
 
-    def readSensor(self, i):
-        if i==0:
-            return self.h["sensor_b"]
-        elif i==1:
-            return self.h["sensor_c"]
+    def readSensor(self, sensor_number):
+        if sensor_number==self.ROT_AXIS:
+            return self.h["sensor_rot"]
+        elif sensor_number==self.BASC_AXIS:
+            return self.h["sensor_basc"]
         else:
             return None
 
