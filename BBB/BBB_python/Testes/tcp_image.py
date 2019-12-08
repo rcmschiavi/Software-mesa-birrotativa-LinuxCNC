6be8f88a31feca87c35b5475877d88b16b6150d4
Lucas C <lucdf>
2019-12-07 22:31:31 -0300

    TCP image funcional

--- BBB/BBB_python/Testes/tcp_image.py
+++ BBB/BBB_python/Testes/tcp_image.py
@@ -7,6 +7,9 @@ Exemplo que só recebe um dado e fecha a conexão
 '''
 
 import socket
+import json
+import numpy
+import time
 import cv2
 import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,16 +22,19 @@ sock.listen(1)
 connection, client_address = sock.accept()
 
 try:
-    img = cv2.imread('./image.jpg', 0)
-    data = {    "mode": "IMAGE_INSPECTION", "params": img}
-    while True:
-        #Espera qualquer coisa e envia o dado
-        data = connection.recv(16)
-        if data:
-            connection.sendall(data)
-        else:
-            break
+    img = cv2.imread('image.jpg', 0)
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    result, imgencode = cv2.imencode('.jpg', img, encode_param)
+    data = numpy.array(imgencode)
+    stringData = data.tostring()
+    data = {"mode": "FRAME", "params": len(stringData)}
+    print "enviando JSON"
+    data = json.dumps(data)
+    connection.sendall(str(data))
+    connection.sendall(stringData)
+
 
 
 finally:
+    "conexão fechada"
     connection.close()
